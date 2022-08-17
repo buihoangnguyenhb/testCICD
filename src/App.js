8cd9087f02c60ea2c2ad1b2291d4b81686af9ea5
nguyenbh <buihoangngnguyenhb@gmail.com>
2022-08-17 11:09:33 +0700

    push code is first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PageWrapper from './component/layout';
+
+function App() {
+  return (
+    <div className="App">
+      <PageWrapper />
+    </div>
+  );
+}
+
+export default App;
